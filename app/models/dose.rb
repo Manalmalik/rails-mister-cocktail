84abd2dbcc71992c72d4081d4a42e4d8aec7f14f
manalmalik <manalmalik.cs@gmail.com>
2020-08-13 12:57:19 +0200

    Validate models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktail_id
-  belongs_to :ingredient_id
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail_id, scope: [:ingredient_id]
 end
